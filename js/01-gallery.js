c33c3af00ecff64e22c4b96d4b460f99f55b0bc8
SpaceRider <spacerider.ua@gmail.com>
2023-07-11 21:16:24 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryCell = galleryItems.map((img, ind) => {
+   return `<li class = "gallery__item">
+   <a href="${img.original}" class = "gallery__link">
+      <img class = "gallery__image" data-index="${ind}" 
+      src="${img.preview}" alt="${image.description}"
+   </a>
+   </li>`;
+})
+
+const listTmpl = `
+   <div class="image-list">${galleryCell.joim('')}</div>`;
+const galleryLst = document.querySelector(".gallery");
+galleryLst.insertAdjacentHTML("beforeend", listTmpl);
+
+galleryLst.addEventListener('click', showOriginalImage);
+
 console.log(galleryItems);
