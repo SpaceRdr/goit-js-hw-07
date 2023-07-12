34eb1bf432adf0adc928d1828c48b676485fffef
SpaceRider <spacerider.ua@gmail.com>
2023-07-12 11:59:54 +0300

    ee

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -11,7 +11,7 @@ const galleryCell = galleryItems.map((img, ind) => {
 })
 
 const listTmpl = `
-   <div class="image-list">${galleryCell.joim('')}</div>`;
+   <div class="image-list">${galleryCell.join('')}</div>`;
    
 const galleryLst = document.querySelector(".gallery");
 galleryLst.insertAdjacentHTML("beforeend", listTmpl);
