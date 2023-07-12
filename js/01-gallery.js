4d99074628c7590ad0042c46114f7b624f650f8e
SpaceRider <spacerider.ua@gmail.com>
2023-07-12 13:01:17 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -17,26 +17,26 @@ const listTmpl = `
 const galleryLst = document.querySelector(".gallery");
 galleryLst.insertAdjacentHTML("beforeend", listTmpl);
 
-galleryLst.addEventListener('click', showOriginalImg);
+// galleryLst.addEventListener('click', showOriginalImg);
 
-function showOriginalImg(event) {
-   console.dir("event.target ", event.target);
-   event.preventDefault();
-   console.log("event.target.nodeName ", event.target.nodeName);
-   if(event.target.nodeName !== "IMG") return ;
+// function showOriginalImg(event) {
+//    console.dir("event.target ", event.target);
+//    event.preventDefault();
+//    console.log("event.target.nodeName ", event.target.nodeName);
+//    if(event.target.nodeName !== "IMG") return ;
 
-   const selectImg = event.target.dataset.index;
-   console.log("event.target.dataset.index", event.target.dataset.index);
-   const examp = basicLightbox.create(
-      `<img src="${selectImg}" width="800">`
-   );
-   console.log("examp", examp);
+//    const selectImg = event.target.dataset.index;
+//    console.log("event.target.dataset.index", event.target.dataset.index);
+//    const examp = basicLightbox.create(
+//       `<img src="${selectImg}" width="800">`
+//    );
+//    console.log("examp", examp);
 
-   examp.show();
-   galleryLst.addEventListener("keydown", (evt) => {
-      if(evt.code === 'Escape') examp.close();
+//    examp.show();
+//    galleryLst.addEventListener("keydown", (evt) => {
+//       if(evt.code === 'Escape') examp.close();
 
-   });
-}
+//    });
+// }
 
 console.log(galleryItems);
