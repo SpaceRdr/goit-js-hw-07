b32edb747bd5989fe53b5d259f7544c2b4c28adc
SpaceRider <spacerider.ua@gmail.com>
2023-07-12 12:38:50 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
+console.log(galleryItems);
 const galleryCell = galleryItems.map((img, ind) => {
    return `<li class = "gallery__item">
    <a href="${img.original}" class = "gallery__link">
@@ -9,6 +9,7 @@ const galleryCell = galleryItems.map((img, ind) => {
    </a>
    </li>`;
 })
+console.log(galleryCell);
 
 const listTmpl = `
    <div class="image-list">${galleryCell.join('')}</div>`;
@@ -21,13 +22,13 @@ galleryLst.addEventListener('click', showOriginalImg);
 function showOriginalImg(event) {
    console.dir(event.target);
    event.preventDefault();
-   
+   console.log(event.target.nodeName);
    if(event.target.nodeName !== "IMG") return ;
 
    const selectImg = event.target.dataset.index;
-
+   console.log(event.target.dataset.index);
    const examp = basicLightbox.create(
-      `<img src="${selectImg}" width="800">`
+      `<img src="${selectImg}" width="320">`
    );
 
    examp.show();
